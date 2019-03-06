06cd782eb303c3b0f25cefb47142a1dcfa39542f
RabihKhatib <rabih.khatib@gmail.com>
2019-03-06 10:36:24 +0200

    fix syntax warning

--- CrimsonCoward/js/quicksand/setting.js
+++ CrimsonCoward/js/quicksand/setting.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
 
 if (jQuery().quicksand) {
 
- 	// Clone applications to get a second collection
+    // Clone applications to get a second collection
 	var $data = $(".portfolio-area").clone();
 	
 	//NOTE: Only filter on the main portfolio page, not on the subcategory pages
@@ -11,16 +11,16 @@ if (jQuery().quicksand) {
 		$(".filter li").removeClass("active");	
 		// Use the last category class as the category to filter by. This means that multiple categories are not supported (yet)
 		var filterClass=$(this).attr('class').split(' ').slice(-1)[0];
-		
-		if (filterClass == 'all') {
-			var $filteredData = $data.find('.item-thumbs');
+        var $filteredData;
+		if (filterClass === 'all') {
+			$filteredData = $data.find('.item-thumbs');
 		} else {
-			var $filteredData = $data.find('.item-thumbs[data-type=' + filterClass + ']');
+			$filteredData = $data.find('.item-thumbs[data-type=' + filterClass + ']');
 		}
-		$(".portfolio-area").quicksand($filteredData, {
-			duration: 600,
-			adjustHeight: 'auto'
-		}	
+        $(".portfolio-area").quicksand($filteredData, {
+            duration: 600,
+            adjustHeight: 'auto'
+        });	
 		$(this).addClass("active"); 			
 		return false;
 	});
